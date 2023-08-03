d154db4aa50fcffd85e8b9c02deb31e6174a867a
Alvinyw <alvin.yiwen@gmail.com>
2023-08-03 09:15:12 +0800

    去除冗余代码

--- src/main.js
+++ src/main.js
@@ -8,19 +8,12 @@ import ElementUI from 'element-ui'
 import '@/styles/theme/index.css'
 Vue.use(ElementUI)
 
-// import './utils/rem' // 适配移动端设备
 import './api' // 接口文件
 import './utils' // 工具库
-import './components/install' // 自定义全局组件
 import './directive' // 自定义全局指令
 import './filters' // 自定义全局过滤器
 import './styles/index.scss' // 全局样式文件
-// import './mock'
 import './permission'
-
-// 引入 SVG 动画
-import lottie from 'vue-lottie';
-Vue.component('lottie', lottie)
 
 Vue.config.productionTip = false
 
